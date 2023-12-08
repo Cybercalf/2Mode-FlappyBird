8cdcc371cd57d30a9fcc420c42b229aa1e1f4c75
Cybercalf <cybercalf@163.com>
2023-12-08 22:11:22 +0800

    delete scripts

--- play.sh
+++
@@ -1,5 +0,0 @@
-# play flappy bird with pretrained model
-# you canchange `model_best.pth.tar` to your pretrained model file name 
-python main.py\
-    --weight model_best.pth.tar\
-    --cuda   # uncomment when gpu is available
